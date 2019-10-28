461bc647e42812dd690f3b71965fe5c4e1ffc1c5
ShingFei <0915328@hr.nl>
2019-10-28 15:28:20 +0100

    removed conflicting statements

--- Serial_python/run.py
+++ Serial_python/run.py
@@ -2,19 +2,12 @@ from main import Wiegand
 VALID_FACILITY_CODES = [ '123']
 VALID_CARDS = [ '12345' ]
 
-GREEN_LED = Pin(0)
-RED_LED = Pin(1)
-
 WIEGAND_ZERO = 0  # Pin number here
 WIEGAND_ONE = 1   # Pin number here
 
 def on_card(card_number, facility_code, cards_read):
 	if (card_number in VALID_CARDS) and (facility_code in VALID_FACILITY_CODES):
-		GREEN_LED.high()
-		RED_LED.low()
-	else:
-		RED_LED.high()
-		GREEN_LED.low()
+		print('succes')
 
 
 Wiegand(WIEGAND_ZERO, WIEGAND_ONE, on_card)
