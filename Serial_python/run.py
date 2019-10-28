c9e9e2a68837acaeae7371aa5633303db774fff1
ShingFei <0915328@hr.nl>
2019-10-28 15:33:06 +0100

    changes on capital fields

--- Serial_python/run.py
+++ Serial_python/run.py
@@ -1,13 +1,12 @@
 from main import Wiegand
-VALID_FACILITY_CODES = [ '123']
-VALID_CARDS = [ '12345' ]
+valid_facility_codes = [ '123']
+valid_cards = [ '12345' ]
 
-WIEGAND_ZERO = 0  # Pin number here
-WIEGAND_ONE = 1   # Pin number here
+wiegand_zero = 0  # Pin number here
+wiegand_one = 1   # Pin number here
 
 def on_card(card_number, facility_code, cards_read):
-	if (card_number in VALID_CARDS) and (facility_code in VALID_FACILITY_CODES):
+	if (card_number in valid_cards) and (facility_code in valid_facility_codes):
 		print('succes')
 
-
-Wiegand(WIEGAND_ZERO, WIEGAND_ONE, on_card)
+Wiegand(wiegand_zero, wiegand_one, on_card)
