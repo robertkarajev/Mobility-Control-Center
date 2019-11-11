4516aae77e174dfbca67e759dfde4050ce482e74
ShingFei <0915328@hr.nl>
2019-11-11 11:26:01 +0100

    fix for the previous push

--- Wiegand_Python_Raspberry/wiegand_read_experimental.py
+++ Wiegand_Python_Raspberry/wiegand_read_experimental.py
@@ -50,7 +50,8 @@ class Wiegand:
 		if self.bits:
 			self.timeout = self.timeout - 1
 			tm.sleep(0.001)
-			if len(self.bits) >= 26 and self.timeout == 0:
+			
+			if len(self.bits) >= 1 and self.timeout == 0:
 					print(len(self.bits))
 					result = int(str(self.bits),2)
 					if result > 1:
