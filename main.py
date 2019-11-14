f3d1a8886a9db81e5c26efc85f59d55961baf4c6
ShingFei <0915328@hr.nl>
2019-11-14 16:35:15 +0100

    main and rfid reader

---
+++ main.py
@@ -0,0 +1,22 @@
+#import wiegand_read_v3 as wg
+from client import MQTTClient
+import rfid_reader as wg
+
+class Main:
+	
+	def __init__(self, path = ''):
+		self.mqtt = MQTTClient("145.24.222.194", True) #boolean is localhost // ip local host, host port 1883 
+		self.verifier = wg.ParkingVerifier([])
+	
+	def run(self):
+		while True:
+			# receive_tag_id = wg.Wiegand.run()
+			receive_tag_id = str(input())
+			if not self.verifier.verify_path(receive_tag_id):
+				path = self.mqtt.getPath(receive_tag_id,'get') # Returns a list
+				self.verifier.change_path(path)
+				self.verifier.verify_path(receive_tag_id)
+
+main = Main()
+
+main.run()
