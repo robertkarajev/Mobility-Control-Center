bf0bb6f237e3249c054ea2e681226171772fbd1e
DESKTOP-33RVDNI\Robert <0851997@hr.nl>
2019-11-18 16:58:06 +0100

    fixed indent for sure

--- client.py
+++ client.py
@@ -5,60 +5,60 @@ import json
 #import main
 
 class MQTTClient:
-    # create a random string containing letters and numbers with a variable length
-    def randomString(self, stringLength):
-        letters = string.ascii_letters + '0123456789'
-        return ''.join(random.choice(letters) for i in range(stringLength))
+	# create a random string containing letters and numbers with a variable length
+	def randomString(self, stringLength):
+		letters = string.ascii_letters + '0123456789'
+		return ''.join(random.choice(letters) for i in range(stringLength))
 
-    # callback function for when the script get a CONNACK from the broker
-    # subscribes to own name so it can receive personal messages from the server-client
-    def on_connect(self, client, userdata, flags, rc):
-        client.subscribe(self.name, 1)
-        if rc == 0:
-            print("Connected to broker")
-            global Connected                # Use global variable
-            Connected = True                # Signal connection
-        else:
-            print("Connection failed")
+	# callback function for when the script get a CONNACK from the broker
+	# subscribes to own name so it can receive personal messages from the server-client
+	def on_connect(self, client, userdata, flags, rc):
+		client.subscribe(self.name, 1)
+		if rc == 0:
+			print("Connected to broker")
+			global Connected                # Use global variable
+			Connected = True                # Signal connection
+		else:
+			print("Connection failed")
 
-    # callback function for when a message is received from the broker
-    def on_message(self, client, userdata, message):
-        if message.topic == self.name:
-            msg = json.loads(str(message.payload.decode('utf-8')))
-            if not isinstance(msg, list):
-                if not self.authorized:
-                    if msg:  # msg == True
-                        self.authorized = True
-                        print('ID authorized by server')
-                    else:
-                        print('ID not authorized by server')
-                        self.client.unsubscribe(self.name)
-                        self.name = self.randomString(4)
-                        self.client.subscribe(self.name, 1)
-                        self.getAuth()
-                else:
-                    print('[ERROR] message received not a list')
-            else:
-                self.msg = msg
-        else:
-            print('message that was not intended for you has been received')
+	# callback function for when a message is received from the broker
+	def on_message(self, client, userdata, message):
+		if message.topic == self.name:
+			msg = json.loads(str(message.payload.decode('utf-8')))
+			if not isinstance(msg, list):
+				if not self.authorized:
+					if msg:  # msg == True
+						self.authorized = True
+						print('ID authorized by server')
+					else:
+						print('ID not authorized by server')
+						self.client.unsubscribe(self.name)
+						self.name = self.randomString(4)
+						self.client.subscribe(self.name, 1)
+						self.getAuth()
+				else:
+					print('[ERROR] message received not a list')
+			else:
+				self.msg = msg
+		else:
+			print('message that was not intended for you has been received')
 
-    # general sending method
-    def sendPublish(self, topic, message, qos):
-        self.client.publish(topic, json.dumps(message), qos)
+	# general sending method
+	def sendPublish(self, topic, message, qos):
+		self.client.publish(topic, json.dumps(message), qos)
 
-    def getAuth(self):
-        self.sendPublish('AU', self.name, 1)
+	def getAuth(self):
+		self.sendPublish('AU', self.name, 1)
 
-    # get a path by sending the name of the car as well as the RFID tag just read(GetPath)
-    def getPath(self, tagId, msg):
-        self.msg = msg
-        self.sendPublish('GP', self.name + ',' + str(tagId), 1)
-        while self.msg == 'get':
-            pass
-        return self.msg
+	# get a path by sending the name of the car as well as the RFID tag just read(GetPath)
+	def getPath(self, tagId, msg):
+		self.msg = msg
+		self.sendPublish('GP', self.name + ',' + str(tagId), 1)
+		while self.msg == 'get':
+			pass
+		return self.msg
 
-    # confirm that you have arrived at the destination (ParkArrived)
+	# confirm that you have arrived at the destination (ParkArrived)
 	def arrived(self):
 		self.sendPublish('PA', self.name, 1)
 
@@ -87,7 +87,7 @@ class MQTTClient:
 			self.client.loop_start()
 		except:
 			print('could not connect, continue trying')
-        #self.client.loop_start()  # start the loop
+		#self.client.loop_start()  # start the loop
 		self.getAuth()
 
 
@@ -95,7 +95,7 @@ class MQTTClient:
 mqttclient = MQTTClient("145.24.222.194", True)
 '''
 while True:
-    print('Enter new RFID tag: ')
-    tagRead = str(input())
-    mqttclient.getPath(tagRead, 'get')  # raceconditions watch out watch out watch out
+	print('Enter new RFID tag: ')
+	tagRead = str(input())
+	mqttclient.getPath(tagRead, 'get')  # raceconditions watch out watch out watch out
 '''
