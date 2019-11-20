062c6c704a18233b23e480a833d851696a941828
ShingFei <0915328@hr.nl>
2019-11-20 12:37:48 +0100

    read file added and added ini file on drive

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-#import wiegand_read_v3 as wg
 from client import MQTTClient
 import rfid_reader as wg
+import configparser as cp
 
-class Main:
-	def __init__(self, path = ''):
-		# Leaving it blank will use local host, host port 1883 
-		self.mqtt = MQTTClient("145.24.222.194", 1883) # host adress, port 
-		self.verifier = wg.ParkingVerifier([])
+read_file = cp.ConfigParser()
+read_file.read('read_file.ini')
+user = read_file['serverinformation']['User']
+pw = read_file['serverinformation']['PW']
+ip = read_file['serverinformation']['IP']
+port = int(read_file['serverport']['MySQl'])
+
+# Leaving it blank will use local host, host port 1883 
+#mqtt = MQTTClient(ip, port) # host adress, port 
+verifier = wg.ParkingVerifier([])
 	
-	def run(self):
-		receive_data = wg.Wiegand()
-		while True:
-			receive_tag_id = receive_data.run()
-			if receive_tag_id != None:
-				print("Card ID: ", receive_tag_id)
-			if not self.verifier.verify_path(receive_tag_id):
-				path = self.mqtt.getPath(receive_tag_id,'get') # Returns a list
-				self.verifier.change_path(path)
-				self.verifier.verify_path(receive_tag_id)
-
-main = Main()
-
-main.run()
+def main():
+	receive_data = wg.Wiegand()
+	while True:
+		receive_tag_id = receive_data.run()
+		if receive_tag_id != None:
+			print("Card ID: ", receive_tag_id)
+		if not verifier.verify_path(receive_tag_id):
+			path = mqtt.getPath(receive_tag_id,'get') # Returns a list
+			verifier.change_path(path)
+			verifier.verify_path(receive_tag_id)
+main()
