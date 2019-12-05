78323a337f7f516c4d77a6685cf52544444eee8c
chris <chris_markus_1999@hotmail.com>
2019-12-05 13:02:39 +0100

    added queue

--- server.py
+++ server.py
@@ -2,18 +2,17 @@ import threading
 import paho.mqtt.client as paho
 import json
 
-database = []
-
 # mockservices eg database connection
-class mySQLconnector:
+class MySQLConnector:
     def __init__(self):
+        self.database = []
         self.checkArr = []
 
     def dummyPathfinding(self, carInfo):
         path = ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7']
         return path
 
-    def carArrivedLogic(self, carId):
+    def registerArrival(self, carId):
         # if carId.state == 1||2
         if carId:
             self.deleteCar(carId)
@@ -30,63 +29,79 @@ class mySQLconnector:
         return True
 
     def addTag(self, tag):
-        if tag in database:
+        if tag in self.database:
             return tag + ' already in database'
         else:
-            database.append(tag)
-            print(database)
+            self.database.append(tag)
+            print(self.database)
             return tag + ' added to database'
 
     def deleteCar(self, carId):
-        if carId in database:
-            database.remove(carId)
+        if str(carId) in self.checkArr:
+            self.checkArr.remove(carId)
 
-class MQTTServer:
-    def on_connect(self, client, userdata, flags, rc):
-        self.client.subscribe('GP', 1)  # Get Path
-        self.client.subscribe('LT', 1)  # arrived at Last Tag
-        self.client.subscribe('AU', 1)  # AUthorize
-        self.client.subscribe('RT', 1)  # Read Tag
-        if rc == 0:
-            print("Connected to broker")
-        else:
-            print("Connection failed")
+
+class MqttServerClient:
+    def __init__(self, port, user, password, broker_address='127.0.0.1'):
+        self.event = threading.Event()
+        self.msgArr = []
+
+        self.broker_address = broker_address            # Broker address
+        self.port = port                                     # Broker port
+        self.user = user                                 # Connection username
+        self.password = password   # Connection password
+
+        print(self.broker_address, self.port)
+
+    def createClient(self):
+        self.client = paho.Client("Server")                   # create new instance
+        self.client.username_pw_set(self.user, password=self.password)  # set username and password
+        self.client.on_connect = self.on_connect              # attach function to callback
+        self.client.on_message = self.setMsg                  # attach function to callback
+
+    def startConnection(self):
+        try:
+            self.client.connect(self.broker_address, port=self.port)
+
+            # create new thread to process network traffic
+            self.client.loop_start()
+        except:
+            print('[INFO] Connection failed...')
+
+    def stopConnection(self):
+        self.client.disconnect()
+        self.client.loop_stop()
+
+    def on_connect(self, client, userdata, flags, connectionResult):
+        if connectionResult == 0:
+            print('[INFO] Connection to broker successful')
+            self.client.subscribe('GP', 1)  # Get Path
+            self.client.subscribe('LT', 1)  # arrived at Last Tag
+            self.client.subscribe('AU', 1)  # AUthorize
+            self.client.subscribe('RT', 1)  # Read Tag
+        elif connectionResult == 1:
+            print('[ERROR] Connection refused - incorrect protocol version')
+        elif connectionResult == 2:
+            print('[ERROR] Connection refused - invalid client identifier')
+        elif connectionResult == 3:
+            print('[ERROR] Connection refused - server unavailable')
+        elif connectionResult == 4:
+            print('[ERROR] Connection refused - bad username or password')
+        elif connectionResult == 5:
+            print('[ERROR] Connection refused - not authorised')
 
     def sendPublish(self, topic, message, qos):
         self.client.publish(topic, json.dumps(message), qos)
 
     def setMsg(self, client, userdata, msg):
-        self.topic = msg.topic
-        self.msg = json.loads(str(msg.payload.decode('utf-8')))
-        self.returnMsg.set()
+        topic = msg.topic
+        msg = json.loads(str(msg.payload.decode('utf-8')))
+        self.msgArr.append([topic, msg])
+        self.event.set()
 
     def getMsg(self):
-        self.returnMsg.wait()
-        self.returnMsg.clear()
-        return [self.topic, self.msg]
-
-    def __init__(self, broker_address, port, user, password, test):
-        self.returnMsg = threading.Event()
-        self.msg = ''
-        self.topic = ''
-
-        self.broker_address = broker_address            # Broker address
-        if test:
-            self.broker_address = '127.0.0.1'
-        self.port = port                                     # Broker port
-        self.user = user                                 # Connection username
-        self.password = password   # Connection password
-
-        self.client = paho.Client("Server")             # create new instance
-        self.client.username_pw_set(self.user, password=self.password)  # set username and password
-        self.client.on_connect = self.on_connect              # attach function to callback
-        self.client.on_message = self.setMsg                  # attach function to callback
-
-        print(self.broker_address, self.port)
-
-        try:
-            self.client.connect(self.broker_address, port=port)  # connect to broker
-        except:
-            print('could not connect, continue trying')
-
-        self.client.loop_start()  # start the loop
+        self.event.wait()
+        msg = self.msgArr.pop(0)
+        if not len(self.msgArr) > 0:
+            self.event.clear()
+        return msg
