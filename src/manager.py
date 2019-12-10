fe6010ac2a37cdfa1ac86371faa4dc5815f59097
chris <chris_markus_1999@hotmail.com>
2019-12-10 16:06:14 +0100

    added new methods, rewriten method names

--- src/manager.py
+++ src/manager.py
@@ -3,9 +3,13 @@ import lib.modules.services as sv
 import lib.testmodules.testservices as tsv
 import lib.modules.timer as tm
 import lib.modules.logger as logger
+import lib.modules.pathfinding as pf
 
 logger = logger.Logger(1)
-topMan = 'Manager'
+topMan = 'ParkingManager'
+topCar = 'Car'
+topMsg = 'Messanger'
+topAdd = 'addTag'
 
 class ParkingManager:
 	def __init__(self):
@@ -13,10 +17,10 @@ class ParkingManager:
 		self.mqttServerClient = sv.MqttServerClient(mqttBrokerCredentials[0],
 													mqttBrokerCredentials[1],
 													mqttBrokerCredentials[3],
-													brokerAddress = mqttBrokerCredentials[2])
+													brokerAddress = mqttBrokerCredentials[2],
+													logger=logger)
 		self.mqttServerClient.createClient()
 		self.mqttServerClient.startConnection()
-
 		mySqlDbCredentials = cr.getMySqlDatabaseCredentials()
 		self.mySqlConnector = sv.MySqlConnector(mySqlDbCredentials[0], 
 												mySqlDbCredentials[1],
@@ -26,6 +30,36 @@ class ParkingManager:
 												logger = logger)
 		self.mySqlConnector.startConnection()
 		logger.info('connections established.', topic=topMan)
+		
+		self.spaces = self.stringToTuple(self.mySqlConnector.getParkingSpaces())
+		self.roads = self.stringToTuple(self.mySqlConnector.getParkingRoads())
+		self.spacesAndRoads = self.spaces + self.roads
+
+		self.pathFinder = pf.PathFinder(self.spaces, self.roads)
+
+	def stringToTuple(self, array):
+		newArr = []
+		for (tag,coord) in array:
+			newArr.append((tag,(int(coord[1]),int(coord[3]))))
+		return newArr
+
+	def getCoordinates(self, tag):
+		for x in self.spacesAndRoads:
+			if tag in x:
+				return x[1]
+	
+	def getTag(self, cor):
+		for x in self.spacesAndRoads:
+			if cor in x:
+				return x[0]
+
+	def replaceCoordinatesInPathWithTagIds(self, corPath):
+		for y in range(len(corPath)):
+			for x in self.spacesAndRoads:
+				if corPath[y][0] in x:
+					corPath[y][0] = x[0]
+					break
+		return corPath
 
 	def carAuthentication(self, carId):
 		result = None
@@ -34,80 +68,92 @@ class ParkingManager:
 			result = False  # carId already in db so new car needs to get new id: return false
 		else:
 			self.mySqlConnector.registerCar(carId)
+			logger.info('registered car', carId, 'state as ==arriving==.', topic=topMan)
 			result = True
 		logger.info('car authentication result:', result, topic=topMan)
 		return result
-
-	def spaceAssignment(self, carId):	#prefered assignemnet???
-		pass
 	
-	def getPath(self, startTag, endTag):
-		logger.debug('path: ', '...', topic=topMan)
-		return ['tag1', 'tag2', 'tag3', endTag]
+	def generatePath(self, beginCoordinates, endCoordinates, prevCoordinates):
+		path = self.pathFinder.getPath(beginCoordinates, endCoordinates, prevCoordinates)
+		path = self.replaceCoordinatesInPathWithTagIds(path)
+		logger.info('path generation successful.', topic=topMan)
+		logger.info('generated path :', path, topic=topMan)
+		return path
 
-	def generatePath(self, carId, startTag):
+	def getSpecificPath(self, carId, startTag, prevTag):
+		beginCoordintates = self.getCoordinates(startTag)
+		endCoordinates = None
+
+		prevCor = ''
+		if prevTag:
+			prevCor = self.getCoordinates(prevTag)
+
 		carState = self.mySqlConnector.getCarState(carId)
 		if carState == 'arriving':
 			endTag = self.mySqlConnector.getAssignedCarToSpace(carId)
-			print(endTag)
 			if endTag:
-				endTag = endTag[0][2]
+				endCoordinates = self.getCoordinates(endTag[0][2])
 			else:
-				endTag = self.mySqlConnector.getRandomParkingSpace()[2]
-				self.mySqlConnector.assignCarToSpace(carId, endTag)
+				newParkingSpaceTag = self.mySqlConnector.getRandomParkingSpace()[2]
+				self.mySqlConnector.assignCarToSpace(carId, newParkingSpaceTag)
+				endCoordinates = self.getCoordinates(newParkingSpaceTag)
 			logger.debug('generating arriving path..', topic=topMan)
 		else:
 			if carState == 'parked':
 				self.mySqlConnector.setCarState(carId, 'leaving')
+				logger.info('registered car', carId, 'state as ==leaving==.', topic=topMan)
 				self.mySqlConnector.unassignCarFromSpace(carId)
-			endTag = self.mySqlConnector.getExit()
-			logger.info('generating leaving path..', topic=topMan)
-		return self.getPath(startTag, endTag)
+			endTag = self.mySqlConnector.getExit()[0][1]
+			endCoordinates = self.getCoordinates(endTag)
+			logger.debug('generating leaving path..', topic=topMan)
+
+		return self.generatePath(beginCoordintates, endCoordinates, prevCor)
 
 	#this method is called when the car has arrived to its set(parking_space OR exit) destination
 	def registerArrival(self, carId):
 		carState = self.mySqlConnector.getCarState(carId)
 		if carState == 'arriving':
 			self.mySqlConnector.setCarState(carId, 'parked')
-			logger.info('car:', carId, 'registered as parked', topic=topMan)
+			logger.info('registered car', carId, 'state as ==parked==.', topic=topMan)
 			return 'parked'
 		else:
 			self.mySqlConnector.deleteCar(carId)
-			logger.info('car:', carId, 'unregistered from parking lot', topic=topMan)
+			logger.info('unregistered car', carId, 'from parking lot.', topic=topMan)
 			return 'clearName'
 
 	def processMessage(self):
+		logger.info('listening for messages...', topic=topMsg)
 		genericMessage = self.mqttServerClient.getMsg()
-		print(genericMessage)
 		topic = genericMessage[0]
 		message = genericMessage[1]
 
+		#[!!!] #TEST THIS LOGGING CONTENT !!!
 		# AUthorization (to authorize cars to make sure no car has the same ID)
 		if topic == 'AU':
+			logger.info(message, 'requests authentication...', topic=topCar)
 			self.mqttServerClient.sendPublish(message, self.carAuthentication(message), 1)
 
 		# Get Path (a car wants a path (either to parking space or the exit))
 		elif topic == "GP":
+			logger.info(message, 'requests path...', topic=topCar)
 			carInfo = message.split(',')
-			self.mqttServerClient.sendPublish(carInfo[0], self.generatePath(carInfo[0], carInfo[1]), 1)
+			self.mqttServerClient.sendPublish(carInfo[0], self.getSpecificPath(carInfo[0], carInfo[1], carInfo[2]), 1)
 
 		# Last Tag (the car has arrived at the destination)
 		elif topic == 'LT':
-			print('car ' + message + ' has arrived succesfully')
+			logger.info(message, 'requests to register arrival...', topic=topCar)
 			self.mqttServerClient.sendPublish(message, self.registerArrival(message), 1)
 
 		# MIGRATE THIS PEACE TO TEST/PREPERATION
 		# # Read Tag (to add to database (voor opzet))
 		# elif topic == 'RT':
+		# logger.info('tagId being added to database', topic=topAdd)
 		# 	carInfo = message.split(',')
 		# 	print(carInfo)
 		# 	self.mqttServerClient.sendPublish(carInfo[0], self.addTag(carInfo[1]), 1)
 		else:
 			print('[ERROR]: topic of message not recognised')
 		print()
-
-	def addTag(self, tagId):
-		pass
 
 	#TEST PURPOSES
 	def getSshLocalBindPort(self):
@@ -121,8 +167,8 @@ class ParkingManager:
 		return sshConn.tunnelForwarder.local_bind_port
 
 manager = ParkingManager()
-#while True:
-	#manager.processMessage()
-manager.carAuthentication('rata')
+while True:
+	manager.processMessage()
+#manager.carAuthentication('rata')
 #manager.registerArrival('java')
 #print(manager.registerArrival('lada'))
