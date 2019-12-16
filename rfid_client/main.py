893a36f90728065f4217f8a4da219525331041be
Robert Karajev <0851997@hr.nl>
2019-12-16 15:57:51 +0100

    fixed main

--- rfid_client/main.py
+++ rfid_client/main.py
@@ -3,6 +3,9 @@ import rfid_reader as wg
 import configparser as cp
 import mqttBrokerinfo as mbi
 import local_logger as ll
+import logger
+
+logger = logger.Logger(1)
 
 class Main:
 	
@@ -21,19 +24,19 @@ class Main:
 				self.verifier.verify_path(receive_tag_id)
 
 # Leaving it blank will use local host, host port 1883 
-mqtt = MQTTClient(mbi[0], mbi[1], mbi[2], mbi[3]) # host adress, port, brokername,brokerpass
+mqtt = MQTTClient(mbi[0], mbi[1], mbi[2], mbi[3], logger) # host adress, port, brokername,brokerpass
 verifier = wg.ParkingVerifier([])
 	
 def main():
 	receive_data = wg.Wiegand()
 	log = ll.LocalLogger()
 	while True:
-		receive_tag_id = receive_data.run()
+		receive_tag_id, previous_tag = receive_data.run()
 		if receive_tag_id != None:
 			print("Card ID: ", receive_tag_id)
 
 		if not verifier.verify_path(receive_tag_id):
-			path = mqtt.getPath(receive_tag_id,'get') # Returns a list
+			path = mqtt.getPath(receive_tag_id) # Returns a list
 			log.write_file("arrival",path,"x")
 			verifier.change_path(path)
 			verifier.verify_path(receive_tag_id)
