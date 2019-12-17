760e2cc29c0db4268137eed7515c6866810ff223
ShingFei <0915328@hr.nl>
2019-12-17 13:06:08 +0100

    updated files

--- rfid_client/client_database_add.py
+++ rfid_client/client_database_add.py
@@ -1,15 +1,6 @@
 from client import MQTTClient as mc
 import json
-<<<<<<< HEAD
-import main
-import configpaser as cp
-
-read_file = cp.ConfigParser()
-read_file.read('read_file.ini')
-ip = read_file['serverinformation']['IP']
-=======
 import time
->>>>>>> development
 
 
 class MQTTClient(mc):
@@ -37,42 +28,3 @@ class MQTTClient(mc):
                 counter += 1
                 start = time.time()
         return self.msg
-<<<<<<< HEAD
-
-    def __init__(self, broker_address, localTesting, password='', broker_port=1883):
-        self.name = self.randomString(idLength)
-        self.authorized = False
-        Connected = False  # global variable for the state of the connection
-
-        self.broker_address = broker_address   # Broker address
-        if localTesting:
-            self.broker_address = "127.0.0.1"  # Broker address
-        self.port = broker_port                # Broker port
-        self.user = self.name                  # Connection username
-        self.password = self.randomString(8)   # Connection password
-
-        self.client = mqttClient.Client(self.name)                      # create new instance
-        self.client.username_pw_set(self.user, password=self.password)  # set username and password
-        self.client.on_connect = self.on_connect                        # attach function to callback
-        self.client.on_message = self.on_message                        # attach function to callback
-        self.msg = ''
-
-        print('broker address: ' + self.broker_address)
-
-        try:
-            self.client.connect(self.broker_address, port=self.port)  # connect to broker
-        except:
-            print('could not connect, continue trying')
-
-        self.client.loop_start()  # start the loop
-        self.getAuth()
-
-
-# (broker_ip, localTesting, password='', broker_port='1833')
-mqttclient = MQTTClient(ip, True)
-while True:
-    print('Enter new RFID tag: ')
-    tagRead = str(input())
-    print(mqttclient.sendTag(tagRead, 'get'))
-=======
->>>>>>> development
--- rfid_client/main.py
+++ rfid_client/main.py
@@ -1,42 +1,35 @@
 from client import MQTTClient
 import rfid_reader as wg
 import configparser as cp
-import mqttBrokerinfo as mbi
+import mqttBrokerInfo as mbi
 import local_logger as ll
+import logger
 
-class Main:
-	
-	def __init__(self, path = ''):
-		self.mqtt = MQTTClient("145.24.222.194", False) #boolean is localhost // ip local host, host port 1883 
-		self.verifier = wg.ParkingVerifier([])
-		print('hello there')
-	
-	def run(self):
-		while True:
-			receive_tag_id, previous_tag = wg.Wiegand.run()
-			#receive_tag_id = str(input())
-			if not self.verifier.verify_path(receive_tag_id):
-				path = self.mqtt.getPath(receive_tag_id,'get') # Returns a list
-				self.verifier.change_path(path)
-				self.verifier.verify_path(receive_tag_id)
+logger = logger.Logger(1)
 
-# Leaving it blank will use local host, host port 1883 
-mqtt = MQTTClient(mbi[0], mbi[1], mbi[2], mbi[3]) # host adress, port, brokername,brokerpass
+# Leaving it blank will use local host, host port 1883
+mbi = mbi.getmqttinfo()
+mqtt = MQTTClient(mbi[0], mbi[1], mbi[2], mbi[3], logger) # host adress, port, brokername,brokerpass
+mqtt.createClient()
+mqtt.startConnection()
 verifier = wg.ParkingVerifier([])
-	
+
 def main():
 	receive_data = wg.Wiegand()
 	log = ll.LocalLogger()
 	while True:
-		receive_tag_id = receive_data.run()
+		receive_tag_id, previous_tag = receive_data.run()
 		if receive_tag_id != None:
 			print("Card ID: ", receive_tag_id)
 
 		if not verifier.verify_path(receive_tag_id):
-			path = mqtt.getPath(receive_tag_id,'get') # Returns a list
-			log.write_file("arrival",path,"x")
-			verifier.change_path(path)
-			verifier.verify_path(receive_tag_id)
+			if receive_tag_id:
+				path, directions = mqtt.getPath(receive_tag_id, previous_tag) # Returns a list
+				print(directions)
+				print(path)
+				log.write_file("arrival",path,"x")
+				verifier.change_path(path)
+				verifier.verify_path(receive_tag_id)
 main()
 
 '''
