f66e46d69190d91c7b27442835be089c693abb04
0915328@hr.nl <0915328@hr.nl>
2020-01-05 14:14:47 +0000

    main few minor fixes on reading localfile

--- rfid_client/main.py
+++ rfid_client/main.py
@@ -8,8 +8,8 @@ import os
 
 logger = logger.Logger(1)
 
-example_cards = ["5c6e522e","5c79af3e","5c75a37e","5c716c9e","5c7313de"]
-example_direction = ["here left","here right"]
+#example_cards = ["5c6e522e","5c79af3e","5c75a37e","5c716c9e","5c7313de"]
+#example_direction = ["here left","here right"]
 # Leaving it blank will use local host, host port 1883
 mbi = mbi.getmqttinfo()
 mqtt = MQTTClient(mbi[0], mbi[1], mbi[2], mbi[3], logger) # host adress, port, brokername,brokerpass
@@ -21,6 +21,7 @@ receive_data = wg.Wiegand()
 
 def give_directions(path):
 	message = path[-1]
+	length = ''
 	destination = []
 	directions = []
 	direct = ['V', 'R', 'B', 'L']
@@ -36,8 +37,9 @@ def give_directions(path):
 def get_rfid_tag():
 	next_tag, previous_tag = receive_data.run()
 	if next_tag:
-		local_file.info(next_tag, " Card id: ")
+		local_file.info(next_tag, "Card id: ")
 		return next_tag, previous_tag
+	return next_tag, previous_tag
 
 def commands(command, angle):
 	if command == 'left':
@@ -49,62 +51,62 @@ def commands(command, angle):
 			
 def start_up():
 	local_list = []
-	if local_file.get_content('depature'):
-		for i in (local_file.get_content('depature')[1:]):
+	if local_file.get_content('Depature'):
+		for i in (local_file.get_content('Depature')[1:]):
 			local_list = local_list.append(i['rfid_tag'])
 
-	elif local_file.get_content('arrival'): # if arrival return this path
-		for i in (local_file.get_content('arrival')[1:]):
+	elif local_file.get_content('Arrival'): # if arrival return this path
+		for i in (local_file.get_content('Arrival')[1:]):
 			local_list = local_list.append(i['rfid_tag'])
 	return local_list
 
 def end_reached(state):
-	if state == 'depature':
+	if state == 'Depature':
+		local_file.info(local_file.get_content("Depature")[-1],'End reached, last known rfid: ')
 		local_file.clear_content()
-		local_file.info(local_file.get_content("depature")[-1],'End reached, last known rfid: ')
 
 def main():
 	local_save = start_up()
 	verifier.change_path(local_save)
-	state = 'depature' if local_file.get_content('depature') else 'arrival'
+	state = 'Depature' if local_file.get_content('Depature') else 'Arrival'
 	while True:
 		receive_tag, previous_tag = get_rfid_tag()
 		result = verifier.verify_path(receive_tag)
 
 		if result == 'lastTag':
-			verifier.change_path([])
-			# mqtt.arrivedAtLastTag()
-			end_reached(state)
-			state = 'arrival' if local_file.get_content('depature')[-1] == receive_tag else 'depature'
-
-		if not result and receive_tag:
-			#path, directions = mqtt.getPath(receive_tag_id, previous_tag) # Returns a list
-			path = example_cards
-			directions = example_direction
-			local_file.write_file(state,path)
 			verifier.change_path(path)
-			verifier.verify_path(receive_tag)
+			mqtt.arrivedAtLastTag()
+			end_reached(state)
+			state = 'Depature'
+		
+		if not result:
+			if receive_tag:
+				path, directions = mqtt.getPath(receive_tag_id, previous_tag) # Returns a list
+				#path = example_cards if state == 'Arrival' else ["5c7313de","5c716c9e"]
+				#directions = example_direction
+				local_file.write_file(state,path)
+				verifier.change_path(path)
+				verifier.verify_path(receive_tag)
 			
-			if state == "depature" and receive_tag == local_file.get_content("arrival")[-2]:
-				# stop going in reverse
-				print('stop reverse, try rotating')
-				pass 
+				if state == "Depature" and receive_tag == local_file.get_content("Arrival")[-2]:
+					# stop going in reverse
+					print('stop reverse, try rotating') 
 
-			if receive_tag == local_file.get_content("arrival")[-1]:
-				local_file.info(receive_tag, 'Card id: ')
-				state = "depature"
-				if state == "depature":
-					local_file.write_file(state,path)
-					verifier.change_path(path)
-					# go in reverse till the previous tag has been read
-					local_file.update(verifier.retrieved_path,"Depature path: ")
+				if receive_tag == local_file.get_content("Arrival")[-1]:
+					local_file.info(receive_tag, 'Card id: ')
+					state = "Depature"
+					if state == "Depature":
+						local_file.write_file(state,path)
+						verifier.change_path(path)
+						# go in reverse till the previous tag has been read
+						local_file.update(verifier.retrieved_path,"Depature path: ")
 
-			local_file.update(verifier.retrieved_path,"Arrival path: ")
-			local_file.info(directions,"Directions given: ")
-			give_directions(directions)
+				local_file.update(verifier.retrieved_path,(state+" path: "))
+				local_file.info(directions,"Directions given: ")
+				give_directions(directions)
 			
 		else:
-			local_file.info(verifier.retrieved_path, "Arrival path: ")
+			local_file.info(verifier.retrieved_path, (state+" path: "))
 		
 '''
 def main():
