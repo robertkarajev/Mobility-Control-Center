863a113e151da1b09674b7d3fda06e866df54e15
Kaan Sargit <sargit@Kaans-MacBook-Pro.local>
2020-01-21 20:22:29 +0100

    Hough transform with avereging

--- houghDetect/hough.py
+++ houghDetect/hough.py
@@ -7,12 +7,43 @@ def canny(image):
     canny = cv2.Canny(blur,50,150)
     return canny
 
+def makeCor(image,lineparameter):
+    slope, intercept = lineparameter
+    y1 = image.shape[0]
+    y2= int(y1*(3/5))
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+
+
+    pass
+
+def average_slope_intercept(image,lines):
+    left_fit=[]
+    right_fit=[]
+    
+    for line in lines:
+        x1,y1,x2,y2 =line.reshape(4)
+        parameters = np.polyfit((x1,x2),(y1,y2),1)
+        slope= parameters[0]
+        intercept= parameters[1]
+        if slope < 0 :
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+
+    left_fit_average = np.average(left_fit,0)
+    right_fit_average = np.average(right_fit,0)
+    left_line=makeCor(image,left_fit_average)
+    right_line =makeCor(image,right_fit_average)
+    return np.array([left_line,right_line])
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[(200,height),(1100,height),(550,250)]])
     mask =  np.zeros_like(image)
     cv2.fillPoly(mask,polygons,255)
-    masked_image = cv2.bitwise_and(canny,mask)
+    masked_image = cv2.bitwise_and(image,mask)
     return masked_image
 
 def display_lines(images,lines):
@@ -26,10 +57,11 @@ def display_lines(images,lines):
 
 image= cv2.imread("image.jpg")
 lane_image=np.copy(image)
-canny = canny(lane_image)
-cropped_image=region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image=region_of_interest(canny_image)
 lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=100)
-line_image = display_lines(lane_image,lines)
+averaged_lines = average_slope_intercept(lane_image,lines)
+line_image = display_lines(lane_image,averaged_lines)
 combo = cv2.addWeighted(lane_image,0.8,line_image,1,1)
 cv2.imshow("result",combo)
 cv2.waitKey(0)
