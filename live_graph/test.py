9e3eca37077220f0bcf8449038bd14a5b87d042f
Robert Karajev <@hr.nl>
2020-01-29 15:56:18 +0100

    repository clean up

--- live_graph/test.py
+++
@@ -1,7 +0,0 @@
-array = []
-
-for x in range(0,10):
-    array.append(x)
-
-while True:
-    
--- live_graph/timer.py
+++
@@ -1,8 +0,0 @@
-import time as tm
-class Sleep:
-	def sleep(self, sleep_time = 1): # default is on 1 sec
-		start = tm.time()
-		end_time =  0
-		while (sleep_time > end_time):
-			end_time = tm.time() - start
-			
