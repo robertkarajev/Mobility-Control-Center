ec006ed586a3e5d3c16ce0699677ed9477c276a8
Kaan Sargit <sargit@MacBook-Pro.local>
2020-01-30 13:24:06 +0100

    final final commit for merge

--- houghDetect/hough.py
+++ houghDetect/hough.py
@@ -37,7 +37,7 @@ def average_slope_intercept(image,lines):
 
 def region_of_interest(image):
     height = image.shape[0]
-    polygons = np.array([[(200,height),(1100,height),(550,250)]])
+    polygons = np.array([[(250,height),(1100,height),(550,250)]])
     mask =  np.zeros_like(image)
     cv2.fillPoly(mask,polygons,255)
     masked_image = cv2.bitwise_and(image,mask)
@@ -48,23 +48,27 @@ def display_lines(image,lines):
     if lines is not None:
         for line in lines:
             x1,y1,x2,y2 = line.reshape(4)
-            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,50,120),10) # pink is life
     return line_image
 
 
 cap = cv2.VideoCapture("test2.mp4")
 while (cap.isOpened()):
     try:
-        _,frame=cap.read()
-        canny_image = canny(frame)
-        cropped_image=region_of_interest(canny_image)
-        lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=100)
-        averaged_lines = average_slope_intercept(frame,lines)
-        line_image = display_lines(frame,averaged_lines)
-        combo = cv2.addWeighted(frame,0.8,line_image,1,1)
-        cv2.imshow("result",combo)
+        frame_read_correct,frame=cap.read()
+
+        if frame_read_correct:
+            canny_image = canny(frame)
+            cropped_image=region_of_interest(canny_image)
+            lines = cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=50,maxLineGap=80)
+            averaged_lines = average_slope_intercept(frame,lines)
+            line_image = display_lines(frame,averaged_lines)
+            combo = cv2.addWeighted(frame,0.8,line_image,1,1)
+            cv2.imshow("result",combo)
         if cv2.waitKey(1) == ord('q'):
             break
+        if frame_read_correct is False:
+            print("Frame wordt niet correct gelezen")
     except Exception as e:
         print(e)
 
